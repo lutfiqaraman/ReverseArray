3ae1af666580d615b4caee50fcdf7cc832227757
lutfiqaraman <lutfiqaraman@gmail.com>
2018-03-24 20:27:35 +0200

    Done

--- ReverseArray/Program.cs
+++ ReverseArray/Program.cs
@@ -10,6 +10,24 @@ namespace ReverseArray
     {
         static void Main(string[] args)
         {
+            int[] Array = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("An Array:");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i] + "\t");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("In Reverse order:");
+            for (int m = Array.Length - 1; m >= 0; m--)
+            {
+                Console.Write(Array[m] + "\t");
+            }
+
+            Console.WriteLine("\n");
+            Console.ReadKey();
         }
     }
 }
